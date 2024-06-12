bbc208349071ec961f9665cff63721c9219f70bf
SamDJ2 <168736144+SamDJ2@users.noreply.github.com>
2024-06-12 21:02:38 +0100

    Added about and profile

--- resume-portfolio/src/components/About/About.js
+++ resume-portfolio/src/components/About/About.js
@@ -3,9 +3,22 @@ import styles from './About.module.css';
 
 const About = () => {
     return (
-        <section className={styles.about} id="about">
-            <h2 className={styles.heading}>About</h2>
-            <p className={styles.paragraph}>About section here</p>
+        <section className={styles.aboutProfile} id="about">
+            <div className={styles.about}>
+                <h2 className={styles.heading}>About</h2>
+                <p className={styles.paragraph}>I'm a Computer Science student at the University of Birmingham with a passion for software development.
+                    I have experience in customer service and hospitality,
+                    exponentially growing my teamwork and communication skills.
+                    I'm dedicated to continuous learning and eager to contribute to innovative projects.</p>
+            </div>
+            <div className={styles.profile}>
+                <h2 className={styles.heading}>Profile</h2>
+                <p>Name: Samuel Davies-Jelly <br></br>
+                Ocupation: Student<br></br>
+                Age: 20 <br></br>
+                Location: Birmingham UK
+                </p>
+            </div>
         </section>
     );
 }
--- resume-portfolio/src/components/Experience/Experience.js
+++ resume-portfolio/src/components/Experience/Experience.js
@@ -5,8 +5,29 @@ const Experience = () => {
     return (
         <section className={styles.experience} id="experience">
             <h2 className={styles.heading}>Education and Experience</h2>
-            <p className={styles.paragraph}>work experience details here.</p>
-        </section>
+                <div className={styles.box}>
+                        <div className={styles.chs}>
+                            <div className={styles.title}>
+                                <h3>Crickhowell High School</h3>
+                                <p>2014-2021</p>
+                            </div>
+                        </div>
+
+                        <div className={styles.mcd}>
+                            <div className={styles.title}>
+                                <h3>Mcdonalds</h3>
+                                <p>2021-2023</p>
+                            </div>
+                        </div>
+
+                        <div className={styles.uob}>
+                            <div className={styles.title}>
+                                <h3> University of Birmingham </h3>
+                                <p>2022-2025</p>
+                            </div>
+                        </div>
+                </div>
+            </section>
     );
 }
 
