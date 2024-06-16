ae8393db018c70337a9ff93ef0f00ac3f21bfbf8
SamDJ2 <168736144+SamDJ2@users.noreply.github.com>
2024-06-16 01:32:57 +0100

    Added Skills and Contact Section

--- frontend/src/components/Contact/Contact.js
+++ frontend/src/components/Contact/Contact.js
@@ -3,11 +3,17 @@ import styles from './Contact.module.css';
 
 const Contact = () => {
     return (
-        <section className={styles.contact} id="contact">
-            <h2 className={styles.heading}>Contact</h2>
-            <p className={styles.paragraph}>Contact information or form here.</p>
+        <section id="contact" className={styles.contactSection}>
+            <h2>Contact Me</h2>
+            <div className={styles.contactInfo}>
+                <p><strong>Mr</strong> Samuel Davies-Jelly</p>
+                <p><strong>Email:</strong> Samdj1029@gmail.com</p>
+                <p><strong>Location:</strong> Birmingham, UK</p>
+                <p><strong>LinkedIn:</strong> <a href="https://uk.linkedin.com/in/samuel-davies-jelly-6117291a0" target="_blank" rel="noopener noreferrer">Samuel Davies-Jelly</a></p>
+                <p><strong>GitHub:</strong> <a href="https://github.com/SamDJ2" target="_blank" rel="noopener noreferrer">SamDJ2</a></p>
+            </div>
         </section>
     );
-}
+};
 
 export default Contact;
--- frontend/src/components/Skills/Skills.js
+++ frontend/src/components/Skills/Skills.js
@@ -3,11 +3,46 @@ import styles from './Skills.module.css';
 
 const Skills = () => {
     return (
-        <section className={styles.skills} id="skills">
-            <h2 className={styles.heading}>Skills</h2>
-            <p className={styles.paragraph}>List your skills here.</p>
+        <section id="skills" className={styles.skillsSection}>
+            <h3 className={styles.skillsName}>Skills</h3>
+            <div className={styles.skillsContainer}>
+                <div className={styles.skillCategory}>
+                    <h3>Programming and Markup Languages</h3>
+                    <ul>
+                        <li><strong>Proficient:</strong> Java, JavaScript, HTML, CSS, Python, C/C++</li>
+                        <li><strong>Familiar:</strong> Haskells</li>
+                    </ul>
+                </div>
+                <div className={styles.skillCategory}>
+                    <h3>Frameworks and Technologies</h3>
+                    <ul>
+                        <li><strong>Database:</strong> SQL, PostgreSQL</li>
+                        <li><strong>Backend:</strong> Spring Boot, Node.js</li>
+                        <li><strong>Frontend:</strong> Angular, React.js</li>
+                        <li><strong>Version Control:</strong> Git, GitHub</li>
+
+                    </ul>
+                </div>
+                <div className={styles.skillCategory}>
+                    <h3>Soft Skills</h3>
+                    <ul>
+                        <li>Strong problem-solving abilities</li>
+                        <li>Effective communication</li>
+                        <li>Adaptable</li>
+                        <li>Teamwork and collaboration</li>
+                    </ul>
+                </div>
+                <div className={styles.skillCategory}>
+                    <h3>Other Skills</h3>
+                    <ul>
+                        <li>Building and configuring PCs</li>
+                        <li>Practical experience with CI/CD pipelines</li>
+                        <li>User experience enhancement (adjustable font size, contrast changes)</li>
+                    </ul>
+                </div>
+            </div>
         </section>
     );
-}
+};
 
 export default Skills;
